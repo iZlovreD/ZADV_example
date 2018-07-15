2cdca8e26bbd3af7f315543220f84a2dd89bf873
iZlovreD <iZlovreD@users.noreply.github.com>
2018-07-15 09:51:34 +0300

    update template

--- areas.lua
+++ areas.lua
@@ -1,9 +1,19 @@
+--[[
+
+A little message for you, my dear creator. =)
+If you find this mod useful, enough interesting or at least
+possible to make Factorio more attractable,and you have new
+ideas for expansion - please,share it with me and community on
+forum: https://forums.factorio.com/viewtopic.php?f=190&t=61538
+
+]]--
+
 
 local ret = { ModName = "mod_example", area={} }
 
 -- table of names replacements to add support non vanila items for all areas
 -- @syntax: "original" = "new name"
--- [ !! currently not implemented !! ]
+-- Currently not implemented, use "update_for" option
 ret.replacements = {}
 
 
@@ -19,33 +29,33 @@ ret.area['example assembler'] = {
 	
 	
 	-- blueprint options
-	,probability = 50	-- 5%				-- [default: 100] from 1 to 1000 (*0.1%)
-	,remoteness_min = 10					-- [default: 10] minimal distance in chunks (32x32) from starting point (starting area settings + minimal remotness)
-	,remoteness_max = 20					-- [default: 0] maximal distance in chunks (32x32) from starting edges, 0 for unlimited (starting area settings + maximal remotness)
-	,only_once = true						-- [default: false] if true, only one copy of this area allowed
-	,max_copies = 0							-- [default: 0] negative or zero for unlimited copies, if set to 1 - same as [only_once = true]
-	,ignore_technologies = true				-- [default: true] If false, only if all necessary technologies is learned - area can be placed
-	,force = "neutral"						-- [default: "neutral"] "player", "neutral", "enemy" or custom name to use/create new Force to use for the building
-	,random_direction = true				-- [default: false] When true, area will be randomly rotated to one of four direction (north, east, south, west) each time when placed on surface
-	,force_build = true						-- [default: true] When true, anything that can be built is else nothing is built if any one thing can't be built. When false, all additional options for entities belw will be ignored
-	,finalize_build = true					-- [default: true] Build entities; place ghosts if "false"
-	,force_reveal = true					-- [default: false] If "true" area will be revealed after build
-	,ignore_water = false					-- [default: false] (currently disabled) If "true" ignore water and place entities above them (entities may be unreachable)
-	,ignore_all_collision = false			-- [default: false] (currently disabled) If "truth" ignores all possible collisions and places entities on top of them (performance lags while placing area)
+	,probability = 50	-- 5%				-- [default: 100]		From 1 to 1000 (*0.1%)
+	,remoteness_min = 10					-- [default: 10]		Minimal distance in chunks (32x32) from starting point (starting area settings + minimal remotness)
+	,remoteness_max = 20					-- [default: 0]			Maximal distance in chunks (32x32) from starting edges, 0 for unlimited (starting area settings + maximal remotness)
+	,only_once = true						-- [default: false]		If true, only one copy of this area allowed
+	,max_copies = 0							-- [default: 0]			Negative or zero for unlimited copies, if set to 1 - same as [only_once = true]
+	,ignore_technologies = true				-- [default: true]		If false, only if all necessary technologies is learned - area can be placed
+	,force = "neutral"						-- [default: "neutral"]	"player", "neutral", "enemy" or custom name to use/create new Force to use for the building
+	,random_direction = true				-- [default: false]		When true, area will be randomly rotated to one of four direction (north, east, south, west) each time when placed on surface
+	,force_build = true						-- [default: true]		When true, anything that can be built is else nothing is built if any one thing can't be built. When false, all additional options for entities belw will be ignored
+	,finalize_build = true					-- [default: true]		Build entities; place ghosts if "false"
+	,force_reveal = true					-- [default: false]		If "true" area will be revealed after build
+	,ignore_water = false					-- [default: false]		(currently disabled) If "true" ignore water and place entities above them (entities may be unreachable)
+	,ignore_all_collision = false			-- [default: false]		(currently disabled) If "true" ignores all possible collisions and places entities on top of them (performance lags while placing area)
 	
 	
 	-- additional options will be applied
 	-- for each entity if allowed to do so
 	-- comment/delete line to exclude
-	,dangerous = false						-- [default: false] If true, players can get harmed or even been killed
-	,active = true							-- [default: true] Deactivating an entity will stop all its operations (inserters will stop working)
-	,minable = false						-- [default: true] Not minable entities can still be destroyed
-	,destructible = false					-- [default: true] When the entity is not destructible it can't be damaged
-	,remains = false						-- [default: false] Replace entities with their remains if they have it
-	,health = -1							-- [default: -1 (ignored)] Set health in procentage of the entity. Entities with 0 health can not be attacked. Setting health to higher than max health will set health to max health
-	,operable = false						-- [default: true] Player can't open gui of this entity and he can't quick insert/input stuff in to the entity when it is not operable
-	,order_deconstruction = false			-- [default: false] Sets the entity to be deconstructed by construction robots
-	,rotatable = false						-- [default: true] When entity is not to be rotatable (inserter, transport belt etc), it can't be rotated by player using the R key
+	,dangerous = false						-- [default: false]		If true, players can get harmed or even been killed
+	,active = true							-- [default: true]		Deactivating an entity will stop all its operations (inserters will stop working)
+	,minable = false						-- [default: true]		Not minable entities can still be destroyed
+	,destructible = false					-- [default: true]		When the entity is not destructible it can't be damaged
+	,remains = false						-- [default: false]		Replace entities with their remains if they have it
+	,health = -1							-- [default: -1]		Set health in procentage of the entity. Negative value to ignore option. Entities with 0 health can not be attacked. Setting health to higher than max health will set health to max health
+	,operable = false						-- [default: true]		Player can't open gui of this entity and he can't quick insert/input stuff in to the entity when it is not operable
+	,order_deconstruction = false			-- [default: false]		Sets the entity to be deconstructed by construction robots
+	,rotatable = false						-- [default: true]		When entity is not to be rotatable (inserter, transport belt etc), it can't be rotated by player using the R key
 	
 	
 	-- custom data to pass it into functions
@@ -79,7 +89,7 @@ ret.area['example assembler'] = {
 	-- @param namelist - "false" in none; array of entity names in this blueprint
 	-- @param entitylist - "false" in none; array of entities themselfs builded (if so) on surface ( surface.find_entities_filtered{area=area, name=newarea.names}; )
 	,ScriptForAll = function(rndroll, game, surface, force, area, center, namelist, entitylist, userdata)
-		local ent = surface.create_entity{name="hidden-electric-energy-interface", position=center, force='neutral'}
+		local ent = surface.create_entity{name="hidden-electric-energy-interface", position=center, force=force}
 		ent.electric_buffer_size = 8283	--
 		ent.power_production = 8283		-- Prevent to use it as free energy source
 		ent.power_usage = 0				--
@@ -88,10 +98,10 @@ ret.area['example assembler'] = {
 	-- An array of messages to be printed in the chat after the creation of the area.
 	-- One will be selected randomly. Message ignored if empty.
 	,messages = {
-		{ msg = "", color = {r=0.30, g=0.70, b=1, a=0.8} }, -- { "Message", {Color RGBA} }
-		{ msg = "A little more belts...", color = {r=0.30, g=0.70, b=1, a=0.8} },
-		{ msg = "Moar little belts...", color = {r=0.30, g=0.70, b=1, a=0.8} },
-		{ msg = "One more belts?..", color = {r=0.30, g=0.70, b=1, a=0.8} }
+		{ msg = "", color = {r=0.30, g=0.70, b=1} }, -- { "Message", {Color RGB} }
+		{ msg = "A little more belts...", color = {r=0.30, g=0.70, b=1} },
+		{ msg = "Moar little belts...", color = {r=0.30, g=0.70, b=1} },
+		{ msg = "One more belts?..", color = {r=0.30, g=0.70, b=1} }
 	}
 	
 	
